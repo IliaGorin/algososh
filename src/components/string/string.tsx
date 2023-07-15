d201124d700be507fcbc1c3a11b0cf5420a08b7b
IliaGorin <101461772+IliaGorin@users.noreply.github.com>
2023-07-15 17:28:50 +0300

    test

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import React from 'react';
+import { SolutionLayout } from '../ui/solution-layout/solution-layout';
 
 export const StringComponent: React.FC = () => {
   return (
     <SolutionLayout title="Строка">
-     
+      <p>Алгоритм разворота строки</p>
     </SolutionLayout>
   );
 };
