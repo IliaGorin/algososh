d0bd54727d15ec0bd168915bb2d3ceab61daf215
IliaGorin <101461772+IliaGorin@users.noreply.github.com>
2023-07-23 12:12:37 +0300

    string page is done

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -5,40 +5,61 @@ import { Input } from '../ui/input/input';
 import styles from './string.module.css';
 import { Circle } from '../ui/circle/circle';
 import { DELAY_IN_MS } from '../../constants/delays';
+import { ElementStates } from '../../types/element-states';
+
+export const stateCircle = (
+  currentIndex: number,
+  indexOfRender: number,
+  arr: Array<string | number>
+) => {
+  let arrLength = arr.length - 1;
+  if (
+    indexOfRender < currentIndex ||
+    indexOfRender > arrLength - currentIndex
+  ) {
+    return ElementStates.Modified;
+  }
+  if (
+    indexOfRender === currentIndex ||
+    indexOfRender === arrLength - currentIndex
+  ) {
+    return ElementStates.Changing;
+  }
+  return ElementStates.Default;
+};
 
 export const StringComponent: React.FC = () => {
-  const [inputString, setInputString] = useState<string>('');
-  const [stringToRender, setStringToRender] = useState<string[]>([]);
-  let reverseString: string[] = [];
+  const [inputString, setInputValue] = useState<string>('');
+  const [arrayForRender, setArrayForRender] = useState<Array<string>>([]);
+  const [isloader, setIsLoader] = useState<boolean>(false);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
 
-  function reverse(inputString: string[]): string[] {
-    // const symbolsArr = inputString.split('');
-    if (inputString.length === 1) return inputString;
-    let end = inputString.length - 1;
-    // let middle = Math.floor(symbolsArr.length / 2);
-    let tempElement = inputString[0];
-    inputString[0] = inputString[end];
-    inputString[end] = tempElement;
+  const handleReverse = () => {
+    const symbolsArray = inputString.split('');
+    let endOfArray = symbolsArray.length - 1;
+    let middle = Math.ceil(symbolsArray.length / 2);
 
-    return inputString;
-  }
+    setIsLoader(true);
+    setArrayForRender([...symbolsArray]);
 
-  const array = [
-    ['1', '2', '3', '4', '5'],
-    ['5', '2', '3', '4', '1'],
-    ['5', '4', '3', '2', '1'],
-  ];
+    setCurrentIndex(0);
+    setIsLoader(false);
+    setInputValue('');
+    let delay = DELAY_IN_MS;
 
-  const delay = (time: number) =>
-    new Promise<void>((res) => setTimeout(res, time));
-
-  const handleReverse = async () => {
-    // await delay(DELAY_IN_MS);
-    for (let i = 0; i < 3; i++) {
-      setStringToRender(array[i]);
-      console.log('count ', i);
-      await delay(DELAY_IN_MS);
+    for (let i = 0; i < middle; i++) {
+      setTimeout(() => {
+        [symbolsArray[i], symbolsArray[endOfArray - i]] = [
+          symbolsArray[endOfArray - i],
+          symbolsArray[i],
+        ];
+        setCurrentIndex((i) => i + 1);
+        setArrayForRender([...symbolsArray]);
+      }, 1000 + delay);
+      delay += 1000;
     }
+
+    return symbolsArray;
   };
 
   return (
@@ -46,27 +67,31 @@ export const StringComponent: React.FC = () => {
       <div className={styles.wrapper}>
         <Input
           placeholder="Введите текст"
-          maxLength={11}
           isLimitText={true}
+          maxLength={11}
           onChange={(e: ChangeEvent<HTMLInputElement>) => {
-            setInputString(e.currentTarget.value);
+            setInputValue(e.currentTarget.value);
           }}
-        ></Input>
+          value={inputString}
+          extraClass="mr-6"
+        />
         <Button
           type={'button'}
           text={'Развернуть'}
+          isLoader={isloader}
+          disabled={inputString === ''}
           onClick={(e) => {
             handleReverse();
           }}
-        >
-          Button
-        </Button>
+        />
       </div>
       <ul className={styles.lettersContainer}>
-        {stringToRender.map((symbol, index) => (
-          <li key={index}>
-            <Circle letter={symbol} />
-          </li>
+        {arrayForRender.map((item: string, indexOfRender: number) => (
+          <Circle
+            key={indexOfRender}
+            letter={item}
+            state={stateCircle(currentIndex, indexOfRender, arrayForRender)}
+          />
         ))}
       </ul>
     </SolutionLayout>
