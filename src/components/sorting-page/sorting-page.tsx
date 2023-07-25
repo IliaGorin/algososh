cff02e1fe96f4038840f6f023097070dfffdcc95
IliaGorin <101461772+IliaGorin@users.noreply.github.com>
2023-07-25 18:50:29 +0300

    add sorting method

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,38 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ChangeEvent } from 'react';
 import { SolutionLayout } from '../ui/solution-layout/solution-layout';
 import styles from './sorting.module.css';
 import { ElementStates } from '../../types/element-states';
 import { RadioInput } from '../ui/radio-input/radio-input';
 import { Button } from '../ui/button/button';
 import { Column } from '../ui/column/column';
-import { getRandomArray } from './utils';
-
-export type ArrayNumbers = {
-  value: number;
-  state: ElementStates;
-};
-
-export type Direction = 'ascending' | 'descending';
+import {
+  getRandomArray,
+  ArrayNumbers,
+  makeBubbleSorting,
+  makeSelectionSorting,
+  TypeOfSorting,
+} from './utils';
 
 export const SortingPage: React.FC = () => {
+  const [selectedMethod, setSelectedMethod] =
+    useState<TypeOfSorting>('selection');
   const [arrayForRender, setArrayForRender] = useState<ArrayNumbers[]>([]);
 
   useEffect(() => {
     setArrayForRender(getRandomArray());
   }, []);
 
+  const handleSelectionOfMethod = (e: ChangeEvent<HTMLInputElement>): void => {
+    setSelectedMethod(e.target.value === 'selection' ? 'selection' : 'bubble');
+  };
+
   return (
     <SolutionLayout title="Сортировка массива">
-      <div className={styles.wrapper}>
-        <RadioInput label="Выбор" defaultChecked />
-        <RadioInput label="Пузырёк" />
-        <div className={styles.buttonsWrapper}>
-          <Button text={'По возрастанию'} />
-          <Button text={'По убыванию'} />
-        </div>
+      <form className={styles.mainWrapper}>
+        <RadioInput
+          label="Выбор"
+          defaultChecked
+          value={'selection'}
+          onChange={handleSelectionOfMethod}
+          checked={selectedMethod === 'selection'}
+        />
+        <RadioInput
+          label="Пузырёк"
+          value={'bubble'}
+          onChange={handleSelectionOfMethod}
+          checked={selectedMethod === 'bubble'}
+        />
+        <Button text={'По возрастанию'} />
+        <Button text={'По убыванию'} />
         <Button text={'Новый массив'} />
-      </div>
-      <ul className={styles.showarray}>
+      </form>
+      <ul className={styles.columns}>
         {arrayForRender.map((element: ArrayNumbers, index: number) => (
           <Column index={element.value} state={element.state} key={index} />
         ))}
--- src/components/sorting-page/utils.ts
+++ src/components/sorting-page/utils.ts
@@ -1,5 +1,17 @@
-import { ArrayNumbers } from './sorting-page';
 import { ElementStates } from '../../types/element-states';
+import { Dispatch, SetStateAction } from 'react';
+import { SHORT_DELAY_IN_MS } from '../../constants/delays';
+import { Direction } from '../../types/direction';
+
+export type ArrayNumbers = {
+  value: number;
+  state: ElementStates;
+};
+
+export type TypeOfSorting = 'selection' | 'bubble';
+
+export const delay = (timer: number) =>
+  new Promise<void>((resolve) => setTimeout(resolve, timer));
 
 export const getRandomArray = (): ArrayNumbers[] => {
   const maxLength = 17;
@@ -22,11 +34,78 @@ export const getRandomArray = (): ArrayNumbers[] => {
 };
 
 export const swap = (
-  array: number[],
+  array: ArrayNumbers[],
   firstIndex: number,
   secondIndex: number
 ): void => {
   const temp = array[firstIndex];
   array[firstIndex] = array[secondIndex];
   array[secondIndex] = temp;
+};
+
+export const makeSelectionSorting = async (
+  array: ArrayNumbers[],
+  setArray: Dispatch<SetStateAction<ArrayNumbers[]>>,
+  direction: Direction
+) => {
+  for (let i = 0; i < array.length; i++) {
+    let maxIndex = i;
+    array[maxIndex].state = ElementStates.Changing;
+    setArray([...array]);
+    await delay(SHORT_DELAY_IN_MS);
+    for (let j = i + 1; j < array.length; j++) {
+      array[j].state = ElementStates.Changing;
+      setArray([...array]);
+      await delay(SHORT_DELAY_IN_MS);
+      if (
+        direction === 'ascending'
+          ? array[j].value < array[maxIndex].value
+          : array[j].value > array[maxIndex].value
+      ) {
+        array[maxIndex].state = ElementStates.Default;
+        maxIndex = j;
+        array[j].state = ElementStates.Changing;
+        setArray([...array]);
+        await delay(SHORT_DELAY_IN_MS);
+      }
+    }
+    if (maxIndex !== i) {
+      swap(array, i, maxIndex);
+      array[i].state = ElementStates.Default;
+    }
+    array[maxIndex].state = ElementStates.Default;
+    array[i].state = ElementStates.Modified;
+    setArray([...array]);
+    await delay(SHORT_DELAY_IN_MS);
+  }
+};
+
+export const makeBubbleSorting = async (
+  array: ArrayNumbers[],
+  setArray: Dispatch<SetStateAction<ArrayNumbers[]>>,
+  direction: Direction
+) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      const p = j + 1;
+      array[p].state = ElementStates.Changing;
+      array[j].state = ElementStates.Changing;
+      setArray([...array]);
+      await delay(SHORT_DELAY_IN_MS);
+      if (
+        direction === 'ascending'
+          ? array[j].value > array[p].value
+          : array[j].value < array[p].value
+      ) {
+        swap(array, j, p);
+        setArray([...array]);
+        await delay(SHORT_DELAY_IN_MS);
+      }
+      array[j].state = ElementStates.Default;
+      array[p].state = ElementStates.Default;
+      setArray([...array]);
+    }
+    array[array.length - i - 1].state = ElementStates.Modified;
+    setArray([...array]);
+  }
 };
